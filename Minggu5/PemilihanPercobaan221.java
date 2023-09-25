46d2d7b8564c1750962f6bbeb006ea75d1f653c8
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-09-25 13:27:41 +0700

    Percobaan 2 Modifikasi

--- Minggu5/PemilihanPercobaan221.java
+++ Minggu5/PemilihanPercobaan221.java
@@ -19,7 +19,26 @@ public class PemilihanPercobaan221 {
 
         String message = total < 65 ? "Remidi" : "Tidak remidi";
 
-        System.out.println("Nilai Akhir = " + total + " sehingga " + message);
-        
+        if (total > 80 && total <= 100){
+            System.out.println("Nilai Akhir = " + total + " (A) sehingga " + message);
+        }
+        else if (total > 73 && total <= 80){
+            System.out.println("Nilai Akhir = " + total + " (B+) sehingga " + message);
+        }
+        else if (total > 65 && total <= 73){
+            System.out.println("Nilai Akhir = " + total + " (B) sehingga " + message);
+        }
+        else if (total > 60 && total <= 65){
+            System.out.println("Nilai Akhir = " + total + " (C+) sehingga " + message);
+        }
+        else if (total > 50 && total <= 60){
+            System.out.println("Nilai Akhir = " + total + " (C) sehingga " + message);
+        }
+        else if (total > 39 && total <= 50){
+            System.out.println("Nilai Akhir = " + total + " (D) sehingga " + message);
+        }
+        else{ 
+            System.out.println("Nilai Akhir = " + total + "(E) sehingga " + message);
+        }
     }
 }
