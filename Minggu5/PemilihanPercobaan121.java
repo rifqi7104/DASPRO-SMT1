bf0ac388c97d06a5e34e896b55b17f7fd7abdb66
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-09-25 12:16:30 +0700

    Percobaan 1

--- Minggu5/PemilihanPercobaan121.java
+++ Minggu5/PemilihanPercobaan121.java
@@ -9,13 +9,12 @@ public class PemilihanPercobaan121 {
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
 
+        String hasil;
+
         System.out.print("Masukkan angka : ");
         int angka = input21.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
-
+        hasil = (angka % 2 == 0) ? " Bilangan genap" : " Bilangan ganjil";
+        System.out.println(angka+" adalah "+hasil);
     }
 }
