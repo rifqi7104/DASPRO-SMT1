982abfebab445a66b112f6faeed4b3ff365f75d7
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-10-02 15:14:01 +0700

    Pemilihan2 Modifikasi terakhir

--- Minggu6/Pemilihan2Percobaan121.java
+++ Minggu6/Pemilihan2Percobaan121.java
@@ -14,6 +14,11 @@ public class Pemilihan2Percobaan121 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
             } else { 
                 System.out.println("Bukan Tahun Kabisat");
             }
