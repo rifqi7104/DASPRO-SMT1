a0bca0b5d69814fc8326f716799918c8f5aeac55
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-10-09 14:02:21 +0700

    Jobsheet 7 Percobaan 1

---
+++ Minggu7/ForKelipatan21.java
@@ -0,0 +1,27 @@
+package Minggu7;
+
+import java.util.Scanner;
+
+public class ForKelipatan21 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+
+        int kelipatan,jumlah=0,counter=0;
+        double average;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = input21.nextInt();
+
+        for (int i = 1; i <= 50; i++){
+            if (i % kelipatan == 0){
+                jumlah += i;
+                counter++;
+
+            }
+        }
+        average = (double) jumlah/counter;
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.println("Rerata = " + average);
+    }
+}
