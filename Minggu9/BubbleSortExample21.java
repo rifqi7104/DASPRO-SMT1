55aa1571768e24b28e4c136f2f5db3c7c4ad2ac6
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-11-04 01:00:41 +0700

    Bubble Sort Example Modifikasi

--- Minggu9/BubbleSortExample21.java
+++ Minggu9/BubbleSortExample21.java
@@ -7,13 +7,13 @@ public class BubbleSortExample21 {
 
         for (int i = 0; i < intData.length; i++) {
             for (int j = 1; j < intData.length-i; j++) {
-                if (intData[j-1] > intData[j]) {
+                if (intData[j-1] < intData[j]) {
                     // Swap Elemen
                     temp = intData[j];
                     intData[j] = intData[j-1];
                     intData[j-1] = temp;
                     
-                }   
+                }
             }
         }
         System.out.println("Hasil Pengurutan : ");
