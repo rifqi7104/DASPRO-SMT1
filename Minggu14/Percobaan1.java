8d4af0a4cd2c27b57734e97ceeba2d7d8acdb0a9
rifqi7104 <rifqirizqullah7104@gmail.com>
2023-12-05 09:21:14 +0700

    Jobsheet 14 Percobaan 1

---
+++ Minggu14/Percobaan1.java
@@ -0,0 +1,25 @@
+package Minggu14;
+
+public class Percobaan1 {
+    
+    public static int faktorialRekursif(int n){
+        if (n == 0) {
+            return(1);
+        } else {
+            return (n * faktorialRekursif(n-1));
+        }
+    }
+
+    public static int faktorialIterasi(int n){
+        int faktor = 1;
+        for (int i = n; i >= 1; i--) {
+            faktor = faktor * i;
+        }
+        return faktor;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(faktorialRekursif(5));
+        System.out.println(faktorialIterasi(5));
+    }
+}
